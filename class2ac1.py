d254624a3808a9425b72acc49e70ebff635a1dfe
promitagd-cpu <promitagd@gmail.com>
2026-05-31 10:45:32 +0530

    Add files via upload

---
+++ class2ac1.py
@@ -0,0 +1,9 @@
+print ("Welcome to the world of programmming")
+
+#print a number
+print (4)
+
+#next line after print
+print ("hello python \n")
+print ("hello" ,5)
+print("welcome to" , end=  "*")
